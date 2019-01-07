eb2004517a39a46ec5e66b0b040571f13ffec992
mkubiak <kontakt@mkubiak.com>
2019-01-07 15:40:23 +0100

    english

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,7 @@ const IndexPage = () => (
         <h1>Portfolio</h1>
         <h2 style={{ marginBottom: 72 }}>Marvin Kubiak</h2>
         <p>
-          Hallo! Mein Name ist Marvin Kubiak, <span className="highlight">1990</span> ist meine Lieblingszahl und ich beschäftige mich
-          ständig mit Designdisziplinen wie <span className="highlight">Typografie</span>, <span className="highlight">3D Rendering</span>,{' '}
-          <span className="highlight">Farblehre</span> und sonstigen <span className="highlight">kleinen Dingen</span>, die die Welt ein
-          Stückchen einfacher machen soll. Neuerdings beschäftige ich mit den Prozessen in der{' '}
-          <span className="highlight">User Experience</span> und das Aufziehen eines{' '}
-          <span className="highlight">Visual Design Guidelines</span> auf Basis der Erkenntnisse der UX. Dabei möchte ich dir gerne helfen,{' '}
-          <span className="highlight">schreib mir einfach!</span>
+          Hallo! My name is Marvin Kubiak, I like the number <span className="highlight">1990</span> and I'm passionate with lots of design-related topics such as <span className="highlight">typography,</span> <span className="highlight">3D-rendering,</span>  <span className="highlight">Color therory</span> and other small things, that make the world a bit easier. I focus on good <span className="highlight">User Experience</span> on Webapps, Websites and native mobile screens. For that I build <span className="highlight">modular design systems</span> with a <span className="highlight">good branding for your Company in mind.</span>  Let's talk and see if we can work together to improve your companys <span className="highlight">success by embracing a funtional design.</span> Your users will be thankful.
         </p>
       </TextGrid>
     </Row>
