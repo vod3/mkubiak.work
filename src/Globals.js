babebf6175d696a6835d907773b3583e6da35202
Marvin Kubiak <kontakt@mkubiak.com>
2019-01-21 09:36:38 +0100

    highlight typo color

--- src/Globals.js
+++ src/Globals.js
@@ -61,6 +61,7 @@ export const globalStyles = css`
     margin-bottom: 32px;
     .highlight {
       background: #87ceeb88;
+      color: #bbe1f5;
     }
     @media (min-width: 600px) {
       margin-bottom: 64px;
